28ee4749b03f1e02ffc661875f3d81e1915075ae
danylprokofyev <danyl01000100@gmail.com>
2017-03-18 14:42:06 +0100

    first commit

---
+++ src/java/beans/stukemonEJB.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import entities.Pokemon;
+import entities.Trainer;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+/**
+ *
+ * @author x6095888z
+ */
+@Stateless
+public class stukemonEJB {
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+//ej1
+
+    public boolean insertarEntrenador(Trainer t) {
+        if (!existeEntrenador(t)) {
+            EntityManager em = emf.createEntityManager();
+            em.persist(t);
+            em.close();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean existeEntrenador(Trainer t) {
+        EntityManager em = emf.createEntityManager();
+        Trainer entrenadorEncontrado = em.find(Trainer.class, t.getName());
+        em.close();
+        return entrenadorEncontrado != null;
+    }
+//ej2
+
+    public Trainer encontrarEntrenador(String entrenador) {
+        return (Trainer) emf.createEntityManager().createNamedQuery("Trainer.findByName").setParameter("name", entrenador).getSingleResult();
+    }
+
+    public boolean tieneSeisPokemon(Trainer t) {
+        List<Pokemon> resultado = emf.createEntityManager().createNamedQuery("Pokemon.findByTrainer").setParameter("trainer", t).getResultList();
+        return (resultado.size() <= 5);
+    }
+
+    public boolean insertPoke(Pokemon p) {
+        if (!existePokemon(p)) {
+            EntityManager em = emf.createEntityManager();
+            em.persist(p);
+            em.close();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean existePokemon(Pokemon p) {
+        EntityManager em = emf.createEntityManager();
+        Pokemon pokeEncontrado = em.find(Pokemon.class, p.getName());
+        em.close();
+        return pokeEncontrado != null;
+    }
+
+    public List<Trainer> seleccionaEntrenadoresFaltosPokemon() {
+        List<Trainer> listaEntrenador = emf.createEntityManager().createNamedQuery("Trainer.findAll").getResultList();
+        List<Trainer> entrena = new ArrayList<>();
+        listaEntrenador.stream().filter((entrenadorAhora) -> (entrenadorAhora.getPokemonCollection().size() < 6)).forEachOrdered((entrenadorAhora) -> {
+            entrena.add(entrenadorAhora);
+        });
+        return entrena;
+    }
+
+    //ej3
+    public boolean borrarPokemon(String nombre) {
+        Pokemon p = encontrarPokemon(nombre);
+        EntityManager em = emf.createEntityManager();
+        Pokemon pokemonEncontrado = em.find(Pokemon.class, p.getName());
+        if (existePokemon(pokemonEncontrado)) {
+            em.remove(pokemonEncontrado);
+            em.close();
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public Pokemon encontrarPokemon(String nombre) {
+        return (Pokemon) emf.createEntityManager().createNamedQuery("Pokemon.findByName").setParameter("name", nombre).getSingleResult();
+    }
+    //ej4
+}
---
+++ src/java/servlets/PokemonFuncion.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import beans.stukemonEJB;
+import entities.Pokemon;
+import entities.Trainer;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author x6095888z
+ */
+@WebServlet(name = "PokemonFuncion", urlPatterns = {"/PokemonFuncion"})
+public class PokemonFuncion extends HttpServlet {
+
+    @EJB
+    stukemonEJB ejb;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet PokemonFuncion</title>");
+            out.println("</head>");
+            out.println("<body>");
+            String nombre = request.getParameter("nombre");
+            String tipo = request.getParameter("tipo");
+            String habilidad = request.getParameter("habilidad");
+            Integer ataque = Integer.parseInt(request.getParameter("ataque"));
+            Integer defensa = Integer.parseInt(request.getParameter("defensa"));
+            Integer velocidad = Integer.parseInt(request.getParameter("velocidad"));
+            Integer vida = Integer.parseInt(request.getParameter("vida"));
+            String entrenador = request.getParameter("entrenador");
+            
+            Pokemon p = new Pokemon(nombre, tipo, habilidad, ataque, defensa, velocidad, vida, 0);
+            Trainer t = ejb.encontrarEntrenador(entrenador);
+            p.setTrainer(t);
+            if (!ejb.tieneSeisPokemon(t)) {
+                out.println("<h1>este entrenador ya tiene 6 pokemon</h1>");
+            }else{
+                if (ejb.insertPoke(p)) {
+                    out.println("<h1>Nuevo pokemon creado</h1>");
+                } else {
+                    out.println("<h1>No se ha podido crear el pokemon</h1>");
+                }
+            }
+            out.println("<form action='index.html'><input type='submit' name='volverInicio' value='Ir de vuelta a Inicio'/></form>");
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
